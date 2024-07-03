a9fadc1e3386d9ae457a486c88801da34872dbda
JUCELIO <juceliocoelho2022@gmail.com>
2024-07-03 15:56:00 -0300

    Create Operation

--- src/main/java/DevJucelio/com/encrypt_api/serviced/OperationService.java
+++ src/main/java/DevJucelio/com/encrypt_api/serviced/OperationService.java
@@ -1,4 +1,35 @@
 package DevJucelio.com.encrypt_api.serviced;
 
+import DevJucelio.com.encrypt_api.domain.operation.Operation;
+import DevJucelio.com.encrypt_api.dto.OperationDTO;
+import DevJucelio.com.encrypt_api.repositories.OperationRepository;
+
 public class OperationService {
+
+    private OperationRepository repository;
+    private  EncryptService encryptService;
+
+    public  OperationService(OperationRepository repository,
+                             EncryptService encryptService){
+        this.repository = repository;
+        this.encryptService = encryptService;
+
+    }
+
+    public Operation create(OperationDTO operationDTO){
+        Operation operation = new Operation();
+
+        String userDocumentHashed = this.encryptService.encryptData(operationDTO.userDocument());
+        String creditCardHashed = this.encryptService.encryptData(operationDTO.creditCardToken());
+
+
+        operation.setCreditCardToken(creditCardHashed);
+        operation.setUserDocument(userDocumentHashed);
+        operation.setValue(operationDTO.operationValue());
+
+        this.repository.save(operation);
+
+        return operation;
+    }
+
 }
